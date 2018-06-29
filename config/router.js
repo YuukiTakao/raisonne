260d12b6142c1d81943b16430aca8d7e0a2865e2
vagrant <vagrant@localhost.localdomain>
2018-06-29 02:47:45 +0000

    feat:Introduction of Pug template engine

--- config/router.js
+++ config/router.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '../app/views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
