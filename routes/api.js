9f0e8b2bcd247b69c3de727d38ab88073963aef2
SuguruKawamura <sugurukawamura@SuguruKawamuranoMacBook-Pro.local>
2018-07-31 00:50:47 +0900

    create controller file and cut out the control functions from router

--- routes/api.js
+++ routes/api.js
@@ -1,37 +1,9 @@
 const express = require('express');
-const models = require('../models/')
+const taskapi = require('../controllers/taskapi')
 const router = express.Router();
 
-/* タスクの登録 */
-router.post('/task/regist', (req, res, next) => {
-  const param = req.body;
-  const insertTask = models.tasks.create({
-    title: param.title,
-    start_date: param.start_date,
-    close_date: param.close_date,
-  })
-  insertTask.then((ret) => {
-    res.send('insert succeeded');
-  })
-  insertTask.error((e) => {
-    res.send(e.message);
-  })
-});
-
-/* タスクの削除 */
-router.get('/task/delete/:id', (req, res, next) => {
-  const targetId = req.params.id;
-  const delTask = models.tasks.destroy({
-    where:{
-      id: targetId
-    }
-  })
-  delTask.then((ret) => {
-    res.send('delete succeeded');
-  })
-  delTask.error((e) => {
-    res.send(e.message);
-  })
-})
+router.post('/task/regist', taskapi.regist);
+router.post('/task/update/:id', taskapi.update);
+router.get('/task/delete/:id', taskapi.delete);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,10 @@ const models = require('../models');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     models.tasks.findAll().then(task=> {
-    
+
     var taskArray = JSON.parse(JSON.stringify(task, null, 2));
     res.render('index', { title: 'raisonne', tasks: taskArray})
   });
-});
-
-/* GET home page. */
-router.post('/update', function(req, res, next) {
-  models.tasks
-    .update({completed: Boolean(req.body.task_status)},{ where: { id: req.body.id}}) // {更新内容},{更新対象}
-    .then(result=> {
-      const param = {result:"Hello World !"};
-      res.header('Content-Type', 'application/json; charset=utf-8')
-      res.send(param);
-  })
 });
 
 module.exports = router;
