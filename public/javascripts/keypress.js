e10b9cb030da101593cdb96d9f4c97fb4621d2ea
yukitakao <tkao.bs@gmail.com>
2018-08-09 23:24:41 +0900

    The task update API can also be called with a task name

--- public/javascripts/keypress.js
+++ public/javascripts/keypress.js
@@ -1,10 +1,10 @@
 console.log('read');
 currentFNo = 1;
-function next_form(formName){
+function nextForm(formName, taskId, taskTitle){
     if (event.keyCode == 13){
         currentFNo += 2;
-        
         console.log(currentFNo);
+        postByFetch('/tasks/update/',taskId,taskTitle,null);
         document.forms[0][currentFNo].focus();
 	}
 };
