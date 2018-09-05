eaa667d141c88c26786ff71d0323ec7877111569
Yuuki Takao <takaaaao3@gmail.com>
2018-09-05 03:03:13 +0000

    logger comment out

--- config/logger.js
+++ config/logger.js
@@ -3,25 +3,25 @@ var bunyan = require('bunyan');
 
 var log = bunyan.createLogger({name: 'myapp'});
 var appLogDirectory = __dirname + '/../log/application_log';
-fs.existsSync(appLogDirectory) || fs.mkdirSync(appLogDirectory);
+//fs.existsSync(appLogDirectory) || fs.mkdirSync(appLogDirectory);
 
-module.exports = bunyan.createLogger({
-  src: true,
-  name: 'raisonne',
-  streams: [
-    {
-      level: 'error',
-      type: 'rotating-file',
-      path: appLogDirectory + '/error.log',
-      period: '1d',
-      count: 30
-    },
-    {
-      level: 'info',
-      type: 'rotating-file',
-      path: appLogDirectory + '/application.log',
-      period: '1d',
-      count: 30
-    }
-  ]
-});
+//module.exports = bunyan.createLogger({
+//  src: true,
+//  name: 'raisonne',
+//  streams: [
+//    {
+//      level: 'error',
+//      type: 'rotating-file',
+//      path: appLogDirectory + '/error.log',
+//      period: '1d',
+//      count: 30
+//    },
+//    {
+//      level: 'info',
+//      type: 'rotating-file',
+//      path: appLogDirectory + '/application.log',
+//      period: '1d',
+//      count: 30
+//    }
+//  ]
+//});
