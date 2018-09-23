d99e8474f7f0e3b36ac08b5f711b58bc8695df3a
yukitakao <takaaaao3@gmail.com>
2018-09-23 23:35:03 +0900

    Implement space data acquisition processing

--- controllers/spacecontroller.js
+++ controllers/spacecontroller.js
@@ -5,25 +5,26 @@ let lists = {};
 lists.index = (req, res, next) => {
 
   const targetId = req.params.id;
-
-  models.lists.findAll(
-    {
-      where:{
-        space_id: targetId
-      }
-    }
-  ).then(results=> {
-
-
-    var listsObjArray = JSON.parse(JSON.stringify(results, null, 2));
+  models.spaces.findAll().then(spaceResults => {
     
-    const responseJson = {
-      title: 'raisonne',
-      lists: listsObjArray,
-    };
+    models.lists.findAll(
+      {
+        where:{
+          space_id: targetId,
+        }
+      }
+    ).then(results=> {
+      const listsObjArray = JSON.parse(JSON.stringify(results, null, 2));
+      const spacesObjArray = JSON.parse(JSON.stringify(spaceResults, null, 2));
+      const responseJson = {
+        title: 'raisonne',
+        lists: listsObjArray,
+        spaces: spacesObjArray,
+      };
+      // Data binding to View.
+      res.render('space', responseJson);
+    });
 
-    // ViewにModelのデータを渡す
-    res.render('space', responseJson);
   });
 }
 module.exports = lists;
