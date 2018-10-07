5bbd1eb63cc093a00d96709e8cc0c8e69e0a494d
yukitakao <takaaaao3@gmail.com>
2018-10-07 23:07:38 +0900

    Create users table definition and migration file

---
+++ migrations/20181007135417-create-users.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      passwrod: {
+        type: Sequelize.STRING(32)
+      },
+      status: {
+        type: Sequelize.INTEGER(3)
+      },
+      last_login_date: {
+        type: Sequelize.DATE
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
---
+++ models/users.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var users = sequelize.define('users', {
+    name: DataTypes.STRING,
+    passwrod: DataTypes.STRING(32),
+    status: DataTypes.INTEGER(3),
+    last_login_date: DataTypes.DATE
+  }, {
+    underscored: true,
+  });
+  users.associate = function(models) {
+    // associations can be defined here
+  };
+  return users;
+};
